c297da287a3a408dae09d6cbc0ac5c8664fc682f
samuel922 <onyangosamuel583@gmail.com>
2026-03-22 21:02:12 +0300

    updated the base url to use https

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const API_KEY = "37b48b43"
-const BASE_URL = "http://www.omdbapi.com/"
+const BASE_URL = "https://www.omdbapi.com/"
 let searchResults = []
 
 const searchText = document.getElementById("search-text")
